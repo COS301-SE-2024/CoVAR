c2b4976038f227402c08663a7118227c10d9c1ae
daniel-geerdink <dangeerdink@gmail.com>
2024-05-30 20:59:52 +0200

    Added Login Page Functionality

--- CoVAR-app/src/App.tsx
+++ CoVAR-app/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './login';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<h2>Home Page</h2>} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- CoVAR-app/src/index.tsx
+++ CoVAR-app/src/index.tsx
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;700&display=swap';
+link.rel = 'stylesheet';
+
+document.head.appendChild(link);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );

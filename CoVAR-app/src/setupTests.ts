3f2bb4f0dc29e8e195575db41169224d35e29e4b
Alec Watson <alecwatson15@gmail.com>
2024-06-01 13:36:51 +0200

    added textDeocoder definition

--- CoVAR-app/src/setupTests.ts
+++ CoVAR-app/src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { TextDecoder, TextEncoder } from 'util';
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;

ba4180256d0bd475b4ffcde0ce63999ec592a69a
A Repko <arenrepko7@gmail.com>
2024-06-01 20:53:19 +0200

    Added file uploading and submission

--- CoVAR-app/src/evaluate/evaluate.tsx
+++ CoVAR-app/src/evaluate/evaluate.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Typography, Paper } from '@mui/material';
 import Sidebar from '../sidebar/sidebar';
 import { mainContentStyles } from '../styles/sidebarStyle';
-import { evaluateContainerStyles } from '../styles/evaluateStyle';
+import { dashboardContainerStyles } from '../styles/dashboardStyle';
+import { evaluateContainerStyles, uploadBoxStyles, uploadButtonStyles } from '../styles/evaluateStyle';
 
 const Evaluate: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleSubmitFile = () => {
+    // Handle file submit
+    console.log('File submitted:', selectedFile);
+
+  };
+
   return (
     <Box sx={evaluateContainerStyles}>
       <Sidebar />
       <Box sx={mainContentStyles}>
-        {/* Main content goes here */}
+        <Paper sx={uploadBoxStyles}>
+          <Typography variant="h6" sx={{ color: '#CAD2C5', marginBottom: 2 }}>
+            Upload a Vulnerability Assessment
+          </Typography>
+          <input
+            type="file"
+            accept=".pdf,.csv"
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+            id="file-upload"
+          />
+          <label htmlFor="file-upload">
+            <Button variant="contained" component="span" sx={uploadButtonStyles}>
+              Upload File
+            </Button>
+          </label>
+          {selectedFile && (
+            <>
+              <Typography sx={{ marginTop: 1, color: '#CAD2C5' }} variant="body2">
+                Selected File: {selectedFile.name}
+              </Typography>
+              <Button
+                variant="contained"
+                sx={{ ...uploadButtonStyles, marginTop: 2 }}
+                onClick={handleSubmitFile}
+              >
+                Submit File
+              </Button>
+            </>
+          )}
+        </Paper>
       </Box>
     </Box>
   );

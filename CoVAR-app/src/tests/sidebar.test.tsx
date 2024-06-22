cf69a5386d82ed20feb4ba294f040db1837da8e0
A Repko <arenrepko7@gmail.com>
2024-06-22 20:13:58 +0200

    sidebartest

--- CoVAR-app/src/tests/sidebar.test.tsx
+++ CoVAR-app/src/tests/sidebar.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import Sidebar from '../sidebar/sidebar';
 
+// Mock Firebase services
+
 jest.mock('../sidebar/components/userRole', () => ({
   __esModule: true, // this property makes it work
-  default: jest.fn().mockReturnValue('admin'),
+  default: jest.fn().mockReturnValue('admin'), // Mock the user role
 }));
 
 jest.mock('firebase/app', () => ({
@@ -27,7 +29,17 @@ jest.mock('firebase/firestore', () => ({
   getFirestore: jest.fn(),
 }));
 
+// // Mock the useNavigate hook from react-router-dom
+// jest.mock('react-router-dom', () => ({
+//   ...jest.requireActual('react-router-dom'),
+//   useNavigate: jest.fn(),
+// }));
+
+// const mockNavigate = jest.requireMock('react-router-dom').useNavigate;
+
 describe('Sidebar Component', () => {
+  
+
   test('renders the sidebar with all menu items', () => {
     render(
       <Router>
@@ -41,4 +53,8 @@ describe('Sidebar Component', () => {
     expect(screen.getByText('Settings')).toBeInTheDocument();
     expect(screen.getByText('Logout')).toBeInTheDocument();
   });
+
+  
+
+ 
 });

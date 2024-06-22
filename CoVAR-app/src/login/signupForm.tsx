2881f6b4b2bebfdd0e77c2d0984496575c62df97
Alec Watson <alecwatson15@gmail.com>
2024-06-22 21:43:51 +0200

    fixed signup error

--- CoVAR-app/src/login/signupForm.tsx
+++ CoVAR-app/src/login/signupForm.tsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { useTheme, ThemeProvider } from '@mui/material/styles';
 import { Container, Box, Typography, TextField, Button, Link, CssBaseline, Card } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { doCreateUserWithEmailAndPassword, doSignInWithGoogle } from '../firebase/auth';
 import GoogleIcon from "../icons/GoogleIcon";
+import { doCreateUserWithEmailAndPassword, doSignInWithGoogle } from '../firebase/auth';
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db } from '../firebase/firebaseConfig';
 import axios from 'axios';
@@ -18,7 +18,7 @@ interface User {
 const addUserToFirestore = async (user: User) => {
   try {
     const userRef = doc(db, "user", user.uid); 
-    const userSnapshot = await getDoc(userRef); // Check if the document exists
+    const userSnapshot = await getDoc(userRef);
 
     if (!userSnapshot.exists()) {
       const userData = {
@@ -43,32 +43,53 @@ interface SignupProps {
 }
 
 const Signup: React.FC<SignupProps> = ({ toggleForm }) => {
-  const theme = useTheme(); // Use the theme hook here
+  const theme = useTheme();
   const navigate = useNavigate();
 
   const signInWithGoogle = async () => {
     try {
       const result = await doSignInWithGoogle();
       if (result.user) {
-        const { uid, email} = result.user;
-        await addUserToFirestore(result.user as User); // Ensure to await Firestore addition
-        // Send user data to backend
-        const response = await axios.post('/api/users/create', {
-          uid,
-          email
-      });
-
-      if (response.status === 201) {
+        const { uid, email } = result.user;
+        await addUserToFirestore(result.user as User);
+        const response = await axios.post('/api/users/create', { uid, email });
+        console.log(response);
+        const firebaseToken = await result.user.getIdToken();
+        console.log("Firebase Token:", firebaseToken);
+  
+        const LoginResponse = await axios.post('/api/users/login', {
+          firebaseToken,
+          username: email
+        });
+        console.log("Login response:", LoginResponse);
+  
+        localStorage.setItem('accessToken', LoginResponse.data.accessToken);
+        localStorage.setItem('refreshToken', LoginResponse.data.refreshToken);
+        axios.defaults.headers.common['Authorization'] = `Bearer ${LoginResponse.data.accessToken}`;
+        axios.defaults.headers.post['Content-Type'] = 'application/json';
+        let getUserResponse;
+        try {
+          getUserResponse = await axios.post(
+            '/api/getUser',
+            { accessToken: localStorage.getItem('accessToken') },
+            { headers: { Authorization: `Bearer ${LoginResponse.data.accessToken}` } }
+          );
+          console.log("Get user response:", getUserResponse);
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+          throw error; // Re-throw the error to be caught by the outer catch block
+        }
+        if (getUserResponse.status === 200) {
           navigate('/');
-      } else {
+        } else {
           throw new Error('Failed to create user in PostgreSQL');
+        }
       }
-  }
-} catch (error) {
-  console.error(error);
-  // Handle errors here
-}
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -84,15 +105,11 @@ const Signup: React.FC<SignupProps> = ({ toggleForm }) => {
     try {
       const userCredential = await doCreateUserWithEmailAndPassword(email, password);
       const user = userCredential.user;
-      await addUserToFirestore(user as User); // Ensure to await Firestore addition
-      // Send user data to backend
-      const response = await axios.post('/api/users/create', {
-        uid: user.uid,
-        email: user.email
-      });
+      await addUserToFirestore(user as User);
+      const response = await axios.post('/api/users/create', { uid: user.uid, email: user.email });
 
       if (response.status === 201) {
-        navigate('/'); // Navigate to the home page after successful signup
+        navigate('/');
       } else {
         throw new Error('Failed to create user in PostgreSQL');
       }
@@ -104,19 +121,19 @@ const Signup: React.FC<SignupProps> = ({ toggleForm }) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container maxWidth="xl" sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <Box sx={{ textAlign: 'center', margin: '20px auto', width: '100%' }}>
-          <Typography variant="h1" color="textPrimary" fontWeight={550} gutterBottom >
+      <Container maxWidth="xl" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Box sx={{ textAlign: 'center', marginRight: 'auto', marginLeft: 'auto' }}>
+          <Typography variant="h1" color="textPrimary" fontWeight={550} gutterBottom>
             CoVAR
           </Typography>
-          <LockOutlinedIcon sx={{ fontSize: 150, color: theme.palette.secondary.main }} />
+          <LockOutlinedIcon sx={{ fontSize: 150, color: theme.palette.primary.main }} />
         </Box>
         <Card sx={{ backgroundColor: theme.palette.background.paper, padding: 4, borderRadius: 1, borderStyle: 'solid', borderWidth: 1, borderColor: theme.palette.divider }}>
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Typography variant="h4" component="h2" fontWeight={550} gutterBottom>
               Sign Up
             </Typography>
-            <Box component="form" onSubmit={handleSubmit} sx={{ width: '100%', mt: 1 }}>
+            <Box component="form" sx={{ width: '100%', mt: 1 }} onSubmit={handleSubmit}>
               <TextField
                 margin="normal"
                 required
@@ -190,7 +207,7 @@ const Signup: React.FC<SignupProps> = ({ toggleForm }) => {
                 sx={{ mt: 3, mb: 2, backgroundColor: theme.palette.primary.main }}
                 onClick={signInWithGoogle}
               >
-                <GoogleIcon />Sign Up with Google
+                <GoogleIcon /> Sign Up with Google
               </Button>
               <Box display="flex" justifyContent="center" alignItems="center" mt={2}>
                 <Typography variant="body2" sx={{ color: theme.palette.text.primary }}>

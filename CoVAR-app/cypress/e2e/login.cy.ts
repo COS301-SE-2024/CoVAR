156828822ae06c1cac7a73ff3840d3be3b62ff96
JBlixems <jfmalan123@gmail.com>
2024-07-15 18:23:05 +0200

    Fix login to include all types of users

--- CoVAR-app/cypress/e2e/login.cy.ts
+++ CoVAR-app/cypress/e2e/login.cy.ts
@@ -1,14 +1,9 @@
 describe('Login E2E Tests', () => {
     beforeEach(() => {
       cy.visit('/login') 
+
+      console.log(Cypress.env());
     })
-  
-    it('displays the entire DOM for debugging', () => {
-      cy.document().then((doc) => {
-        const domString = doc.documentElement.outerHTML;
-        cy.task('logDOM', domString);
-      });
-    });
   
     it('displays an error message with incorrect credentials', () => {
       cy.get('input[name="email"]', { timeout: 4000 }).type('wronguser@something.com')
@@ -20,11 +15,12 @@ describe('Login E2E Tests', () => {
     })
   
     it('successfully logs in with correct credentials', () => {
-      cy.get('input[name="email"]', { timeout: 4000 }).type('jfmalan1@gmail.com')
-      cy.get('input[name="password"]').type('Password') 
+      cy.get('input[name="email"]', { timeout: 4000 }).type(Cypress.env('client_username'))
+      cy.get('input[name="password"]').type(Cypress.env('client_password')) 
       cy.get('button[type="submit"]').click() 
   
-      cy.url().should('include', '/') 
+      cy.wait(2500)
+      cy.url().should('include', '/dashboard') 
     })
   
   })

7e219e98ca31b478b4c7fe89b14d71b289a3b521
JBlixems <jfmalan123@gmail.com>
2024-07-17 11:09:48 +0200

    Increase file upload size

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
-app.use(express.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb', extended: true }))
 
 // Test connection to postgres
 pgClient.on('error', err => {

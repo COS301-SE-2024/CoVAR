c572587a2d9589f43d9122285b32791e25487a69
Alec Watson <alecwatson15@gmail.com>
2024-08-08 16:43:20 +0200

    changes for model swapping logic

--- langchain/app.py
+++ langchain/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+import os
+from dotenv import load_dotenv, find_dotenv
+
 import requests
 
 app = Flask(__name__)
@@ -12,23 +16,30 @@ def run_test():
     prompt = ChatPromptTemplate.from_template(template)
 
     # Check if service is running on host machine
-    try:
-        response = requests.get("http://host.docker.internal:11434")
-        response.raise_for_status()
-        print("Service is running")
-    except requests.exceptions.RequestException as e:
-        print(f"Service is not running: {e}")
-        return jsonify({'error': 'Ollama service is not running or inaccessible'}), 500
+    #try:
+    #   response = requests.get("http://host.docker.internal:11434")
+    #    response.raise_for_status()
+    #    print("Service is running")
+    #except requests.exceptions.RequestException as e:
+    #    print(f"Service is not running: {e}")
+    #    return jsonify({'error': 'Ollama service is not running or inaccessible'}), 500
 
     try:
-        model = ChatOllama(model="llama3.1:8b", base_url="http://host.docker.internal:11434")
+        load_dotenv(find_dotenv())
+        model_type = os.environ['MODEL_TYPE']
+        if(model_type == 'openai'):
+            model = ChatOpenAI()
+        elif(model_type == 'ollama'):
+            model = ChatOllama(model="llama3.1:8b", base_url="http://host.docker.internal:11434")
+        else:
+            raise ValueError(f"Invalid model type: {model_type}")
         chain = prompt | model
         result = chain.invoke({"question": "What are CVE's and should issues be stored by that category in networks?"})
         print(f"Model invocation result: {result}")
         return jsonify({'result': result.content})
     except Exception as e:
         print(f"Error invoking model: {e}")
-        return jsonify({'error': f'Error gg invoking model: {e}'}), 500
+        return jsonify({'error': f'Error invoking model: {e}'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000, debug=True) 
+    app.run(host='0.0.0.0', port=6000, debug=True)

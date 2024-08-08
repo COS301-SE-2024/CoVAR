35339ba1835b6d29d10e2d80a67215ca8674f029
Alec Watson <alecwatson15@gmail.com>
2024-08-08 19:35:48 +0200

    change for openai and gemini

--- langchain/app.py
+++ langchain/app.py
@@ -2,10 +2,11 @@ from flask import Flask, request, jsonify
 from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
-import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv, find_dotenv
-
-import requests
+import traceback
+import os
+import openai
 
 app = Flask(__name__)
 
@@ -15,30 +16,33 @@ def run_test():
     template = """Question {question}\nAnswer Let's think step by step."""
     prompt = ChatPromptTemplate.from_template(template)
 
-    # Check if service is running on host machine
-    #try:
-    #   response = requests.get("http://host.docker.internal:11434")
-    #    response.raise_for_status()
-    #    print("Service is running")
-    #except requests.exceptions.RequestException as e:
-    #    print(f"Service is not running: {e}")
-    #    return jsonify({'error': 'Ollama service is not running or inaccessible'}), 500
-
     try:
         load_dotenv(find_dotenv())
         model_type = os.environ['MODEL_TYPE']
-        if(model_type == 'openai'):
+        
+        if model_type == 'openai':
+            openai.api_key = os.environ['API_KEY']
             model = ChatOpenAI()
-        elif(model_type == 'ollama'):
+        
+        elif model_type == 'ollama':
             model = ChatOllama(model="llama3.1:8b", base_url="http://host.docker.internal:11434")
+        
+        elif model_type == 'gemini':
+            if "GOOGLE_API_KEY" not in os.environ:
+                os.environ["GOOGLE_API_KEY"]=os.environ['API_KEY']
+            model = ChatGoogleGenerativeAI(model="gemini-pro")
+        
         else:
             raise ValueError(f"Invalid model type: {model_type}")
+        
         chain = prompt | model
         result = chain.invoke({"question": "What are CVE's and should issues be stored by that category in networks?"})
         print(f"Model invocation result: {result}")
         return jsonify({'result': result.content})
+    
     except Exception as e:
         print(f"Error invoking model: {e}")
+        traceback.print_exc()
         return jsonify({'error': f'Error invoking model: {e}'}), 500
 
 if __name__ == '__main__':

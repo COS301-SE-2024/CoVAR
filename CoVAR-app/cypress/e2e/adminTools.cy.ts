b948881e6da6b30a16d97ba9887295208f1982d1
A Repko <arenrepko7@gmail.com>
2024-09-26 23:03:08 +0200

    fix test

--- CoVAR-app/cypress/e2e/adminTools.cy.ts
+++ CoVAR-app/cypress/e2e/adminTools.cy.ts
@@ -4,13 +4,13 @@ describe('Admin tools E2E Tests', () => {
     })
 
     it('client should not see admin tools or evaluate page', () => {
-        cy.wait(4000);
+        cy.wait(10000);
 
         // Login
-        cy.get('input[name="email"]', { timeout: 4000 }).type(Cypress.env('client_username'), { log: false })
+        cy.get('input[name="email"]', { timeout: 10000 }).type(Cypress.env('client_username'), { log: false })
         cy.get('input[name="password"]').type(Cypress.env('client_password'), { log: false })
         cy.get('button[type="submit"]').click()
-        cy.wait(4000);
+        cy.wait(10000);
 
 
         //Navigate to the admin tools page
@@ -24,19 +24,19 @@ describe('Admin tools E2E Tests', () => {
     })
 
     it('demote a va to client and promote again', () => {
-        cy.wait(4000);
-        cy.wait(4000);
+        cy.wait(10000);
+        cy.wait(10000);
 
         // Login
-        cy.get('input[name="email"]', { timeout: 4000 }).type(Cypress.env('admin_username'), { log: false })
+        cy.get('input[name="email"]', { timeout: 10000 }).type(Cypress.env('admin_username'), { log: false })
         cy.get('input[name="password"]').type(Cypress.env('admin_password'), { log: false })
         cy.get('button[type="submit"]').click()
-        cy.wait(4000);
-        cy.wait(4000);
+        cy.wait(10000);
+        cy.wait(10000);
 
         //Navigate to the admin tools page
         cy.get('[test-id="adminToolsLink"]', { timeout: 4500 }).click();
-        cy.wait(4000);
+        cy.wait(10000);
 
         //Demote and promote
         cy.get('.MuiDataGrid-root .MuiDataGrid-row', { timeout: 8000 }).contains(Cypress.env('va_username'), { log: false }).closest('.MuiDataGrid-row').contains('button', 'Unassign').click();
@@ -50,17 +50,17 @@ describe('Admin tools E2E Tests', () => {
     })
 
     it('assign a client to a va', () => {
-        cy.wait(4000);
+        cy.wait(10000);
 
         // Login as admin
-        cy.get('input[name="email"]', { timeout: 4000 }).type(Cypress.env('admin_username'), { log: false })
+        cy.get('input[name="email"]', { timeout: 10000 }).type(Cypress.env('admin_username'), { log: false })
         cy.get('input[name="password"]').type(Cypress.env('admin_password'), { log: false })
         cy.get('button[type="submit"]').click()
-        cy.wait(4000);
+        cy.wait(10000);
 
         //Navigate to the admin tools page
         cy.get('[test-id="adminToolsLink"]', { timeout: 4500 }).click();
-        cy.wait(4000);
+        cy.wait(10000);
 
         // Assign client to va
         cy.get('.MuiDataGrid-root .MuiDataGrid-row')
@@ -68,11 +68,11 @@ describe('Admin tools E2E Tests', () => {
             .closest('.MuiDataGrid-row')
             .contains('button', 'Assign Client')
             .click();
-            cy.wait(4000);
+            cy.wait(10000);
 
         cy.get('.MuiDialog-root').should('exist');
         const clientUsername = Cypress.env('client_username');
-        cy.wait(4000);
+        cy.wait(10000);
 
         // Type part of the client username and immediately look for the options
         cy.get('.MuiFormControl-root > .MuiInputBase-root')
@@ -82,20 +82,20 @@ describe('Admin tools E2E Tests', () => {
                 // Select the specific client_username from the Autocomplete options
                 cy.get('.MuiAutocomplete-listbox').contains(clientUsername).click();
             });
-            cy.wait(4000);
+            cy.wait(10000);
 
         // Ensure the assign button is visible and click it
         cy.get('.MuiDialog-root').find('button').contains('Assign').click({ force: true });
 
 
-        cy.wait(4000);
+        cy.wait(10000);
 
         //Logout
         cy.get('[test-id="logoutButton"]').click();
-        cy.wait(4000);
+        cy.wait(10000);
 
         //Login as va
-        cy.get('input[name="email"]', { timeout: 4000 }).type(Cypress.env('va_username'), { log: false })
+        cy.get('input[name="email"]', { timeout: 10000 }).type(Cypress.env('va_username'), { log: false })
         cy.get('input[name="password"]').type(Cypress.env('va_password'), { log: false })
         cy.get('button[type="submit"]').click()
 
@@ -105,23 +105,23 @@ describe('Admin tools E2E Tests', () => {
     })
 
     it('remove a client from a va', () => {
-        cy.wait(4000);
+        cy.wait(10000);
         // Login as admin
-        cy.get('input[name="email"]', { timeout: 4000 }).type(Cypress.env('admin_username'), { log: false })
+        cy.get('input[name="email"]', { timeout: 10000 }).type(Cypress.env('admin_username'), { log: false })
         cy.get('input[name="password"]').type(Cypress.env('admin_password'), { log: false })
         cy.get('button[type="submit"]').click()
-        cy.wait(4000);
-        cy.wait(4000);
+        cy.wait(10000);
+        cy.wait(10000);
 
         //Navigate to the organisation page
         cy.get('[test-id="adminToolsLink"]', { timeout: 4500 }).click();
-        cy.wait(4000);
+        cy.wait(10000);
 
         //Remove client from va
         cy.get('.MuiDataGrid-root .MuiDataGrid-row').contains(Cypress.env('va_username'), { log: false }).closest('.MuiDataGrid-row').contains('button', 'Remove Client').click();
         cy.get('.MuiDialog-root').should('exist');
         const clientUsername = Cypress.env('client_username');
-        cy.wait(4000);
+        cy.wait(10000);
 
         // Type part of the client username and immediately look for the options
         cy.get('.MuiFormControl-root > .MuiInputBase-root')
@@ -131,7 +131,7 @@ describe('Admin tools E2E Tests', () => {
                 // Select the specific client_username from the Autocomplete options
                 cy.get('.MuiAutocomplete-listbox').contains(clientUsername).click();
             });
-            cy.wait(4000);
+            cy.wait(10000);
 
         // Ensure the assign button is visible and click it
         cy.get('.MuiDialog-root').find('button').contains('Unassign').click({ force: true });
